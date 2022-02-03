86db6308a03100f616e2e3bbb0808cb9c8e44fc6
Tim Kretschmer <xbigtk13x@gmail.com>
2022-02-02 21:18:26 -0600

    Allow app to go fullscreen. #7

--- GamePicker.gd
+++ GamePicker.gd
@@ -17,7 +17,15 @@ func _ready():
 		var game_button = SC.Chrome.text_button(self, game, "_on_game_chosen", [game])
 		SC.link(game_grid,game_button)		
 		
-	SC.link(game_picker_container,game_grid)
+	var fullscreen_button = SC.Chrome.text_button(self, "Fullscreen", "_on_fullscreen_pressed", [])
+		
+	var vbox = VBoxContainer.new()
+	SC.link(vbox, game_grid)
+	SC.link(vbox, fullscreen_button)
+	SC.link(game_picker_container,vbox)
 
 func _on_game_chosen(game):
 	return get_tree().change_scene("res://companion/" + game + "/" + game + ".tscn")
+
+func _on_fullscreen_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
